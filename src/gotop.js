a970b4329f365546b58bb7d7a706d2fd0dfabffa
redspite <718352984@qq.com>
2019-03-30 02:08:40 +0800

    1.0.6

--- src/gotop.js
+++ src/gotop.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 import img from './gotop.png';
 import './gotop.css';
 
-
+const docuEl = document.documentElement;
 class Gotop extends PureComponent{
   constructor(props){
     super(props)
     this.state = {
       show: false,
       visibilityHeight: props.visibilityHeight ? props.visibilityHeight : 400,
-      target: props.target && props.target() ?  props.target(): window 
+      target: props.target && props.target() ?  props.target(): window ,
+      ele: props.target && props.target() ? props.target(): docuEl
     }
     this.handleScroll = this.handleScroll.bind(this);
     this.goTop = this.goTop.bind(this);
@@ -23,19 +24,19 @@ class Gotop extends PureComponent{
   }
   handleScroll(){
     const minHei = this.state.visibilityHeight;
-    let scrolltop = this.state.target.scrollTop;
+    let scrolltop = this.state.ele.scrollTop;
     let show = false;
     scrolltop > minHei ? show = true : show = false;
     this.setState({show})
   }
   goTop(){
-      const origin_distance = this.state.target.scrollTop;
+      const origin_distance = this.state.ele.scrollTop;
       const Time = 80;
       const v = origin_distance/Time;
       let t = setInterval(()=>{
-        const distance = this.state.target.scrollTop;
-        this.state.target.scrollTop = distance - v;
-        if(this.state.target.scrollTop <= 0) { clearInterval(t)} 
+        const distance = this.state.ele.scrollTop;
+        this.state.ele.scrollTop = distance - v;
+        if(this.state.ele.scrollTop <= 0) { clearInterval(t)} 
       },1);
 
       // firfox,ie
